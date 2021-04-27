6b47e206777e7597770648bac00ac5af2f8018c8
HeshamErfan <37734881+HeshamErfan@users.noreply.github.com>
2021-04-27 16:47:52 +0200

    Adding custom marker

--- README.md
+++ README.md
@@ -42,3 +42,74 @@ import GoogleMaps
 }
 ```
 5. Use the `GoogleMapsWidget.dart` inside the `lib/widget` folder as normal widget and use it where you want.
+
+
+
+
+## Adding Custom Marker To the map
+
+### Adding normal marker
+
+1. Declare a Set of Markers that will be shown on the map
+```dart
+Set<Marker> _markers = Set<Marker>();
+```
+
+2. Add the set of markers to GoogleMap widget
+```dart
+GoogleMap(
+      markers: _markers,
+```
+
+3. Update the set of markers after the map is created in **onMapCreated**
+```dart
+GoogleMap(
+      onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+              _setMapPins([LatLng(30.029585, 31.022356)]);
+            }
+```
+
+4. Using this function the map will be updated with the given markers on it
+```dart
+_setMapPins(List<LatLng> markersLocation) {
+    _markers.clear();
+    setState(() {
+      markersLocation.forEach((markerLocation) {
+        _markers.add(Marker(
+          markerId: MarkerId(markerLocation.toString()),
+          position: markerLocation,
+        ));
+      });
+    });
+  }
+```
+
+### Customizing the markers
+
+1. Declare a BitmapDescriptor which will hold the customIcon
+```dart
+late BitmapDescriptor customIcon;
+```
+
+2. Inside **initState()** Assign the needed png to the **customIcon**
+```dart
+@override
+  void initState() {
+    BitmapDescriptor.fromAssetImage(ImageConfiguration(size: Size(50, 50)),
+            'assets/images/marker_car.png')
+        .then((icon) {
+      customIcon = icon;
+    });
+    super.initState();
+  }
+```
+
+3. Finally add the **customIcon** to the marker
+```dart
+Marker(
+     markerId: MarkerId(markerLocation.toString()),
+     position: markerLocation,
+     icon: customIcon,
+   )
+```
--- lib/Widgets/GoogleMapsWidget.dart
+++ lib/Widgets/GoogleMapsWidget.dart
@@ -9,34 +9,61 @@ class GoogleMapsWidget extends StatefulWidget {
 }
 
 class _GoogleMapsWidgetState extends State<GoogleMapsWidget> {
-
   Completer<GoogleMapController> _controller = Completer();
+
+  Set<Marker> _markers = Set<Marker>();
+  late BitmapDescriptor customIcon;
+
+  @override
+  void initState() {
+    BitmapDescriptor.fromAssetImage(ImageConfiguration(size: Size(50, 50)),
+            'assets/images/marker_car.png')
+        .then((icon) {
+      customIcon = icon;
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
-        mapType: MapType.normal,
-        rotateGesturesEnabled: true,
-        zoomGesturesEnabled: true,
-        trafficEnabled: false,
-        tiltGesturesEnabled: false,
-        scrollGesturesEnabled: true,
-        compassEnabled: true,
-        myLocationButtonEnabled: true,
-        zoomControlsEnabled: false,
-        mapToolbarEnabled: false,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(26.8206, 30.8025),
-          zoom: 14.0,
-        ),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-        onCameraMove: (position) {
-          print(position.zoom);
-          print(position.target.longitude);
-          print(position.target.latitude);
-        },
-      );
+      mapType: MapType.normal,
+      rotateGesturesEnabled: true,
+      zoomGesturesEnabled: true,
+      trafficEnabled: false,
+      tiltGesturesEnabled: false,
+      scrollGesturesEnabled: true,
+      compassEnabled: true,
+      myLocationButtonEnabled: true,
+      zoomControlsEnabled: false,
+      mapToolbarEnabled: false,
+      markers: _markers,
+      initialCameraPosition: CameraPosition(
+        target: LatLng(30.029585, 31.022356),
+        zoom: 14.47,
+      ),
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+        _setMapPins([LatLng(30.029585, 31.022356)]);
+      },
+      onCameraMove: (position) {
+        print(position.zoom);
+        print(position.target.longitude);
+        print(position.target.latitude);
+      },
+    );
+  }
+
+  _setMapPins(List<LatLng> markersLocation) {
+    _markers.clear();
+    setState(() {
+      markersLocation.forEach((markerLocation) {
+        _markers.add(Marker(
+          markerId: MarkerId(markerLocation.toString()),
+          position: markerLocation,
+          icon: customIcon,
+        ));
+      });
+    });
   }
 }
